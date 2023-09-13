264322ce2ea5d0b126423c12f411e68e05c88259
Gabriel <gabrielmarcelino099@gmail.com>
2023-09-13 15:46:56 -0300

    feat: card-product created

--- src/app/icons/icon-cart/icon-cart.component.ts
+++ src/app/icons/icon-cart/icon-cart.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-icon-cart',
   templateUrl: './icon-cart.component.html',
   styleUrls: ['./icon-cart.component.css'],
 })
-export class IconCartComponent {}
+export class IconCartComponent {
+  @Input() fill: string = '';
+}
